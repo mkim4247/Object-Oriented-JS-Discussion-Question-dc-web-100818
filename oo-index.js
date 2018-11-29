555b998f1071fe05df8ab1bcee2a6992766d67ec
Michael Kim <m.kim4247@gmail.com>
2018-11-29 09:35:00 -0500

    Done.

--- oo-index.js
+++ oo-index.js
@@ -5,4 +5,61 @@ class Unicorn {
     this.health = 1000
   }
 
+  restart(){
+    while (eventArea.hasChildNodes()) {
+        eventArea.removeChild(eventArea.lastChild);
+    }
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("Out on the street, ready to go!"));
+    eventArea.prepend(div);
+    imageArea.className = 'new';
+  }
+
+  attack(){
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("'ATTAAAAAAAAAK!''"));
+    eventArea.prepend(div);
+    imageArea.className = 'attack';
+  }
+
+  defend() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You put sunglasses on."));
+    eventArea.prepend(div);
+    imageArea.className = 'defend';
+  }
+
+  heal() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You feel refreshed after yoga."));
+    eventArea.prepend(div);
+    imageArea.className = 'heal';
+  }
+
+  breakdance() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("It's really spinning class"));
+    eventArea.prepend(div);
+    imageArea.className = 'breakdance';
+  }
+
+  eatCronut() {
+    var div = document.createElement("div");
+    div.appendChild(document.createTextNode("You've gained 1000 health points."));
+    eventArea.prepend(div);
+    imageArea.className = 'eat';
+  }
+
 }
+//
+
+let lane = new Unicorn('Lane', 'FRIEND')
+
+const eventArea = document.getElementById('event_area')
+const imageArea = document.getElementById('image_area')
+const restartButton = document.getElementById('restart').addEventListener("click",()=> lane.restart())
+const attackButton = document.getElementById('attack').addEventListener("click",()=> lane.attack())
+const defendButton = document.getElementById('defend').addEventListener("click",()=> lane.defend())
+const healButton = document.getElementById('heal').addEventListener("click",()=> lane.heal())
+const breakdanceButton = document.getElementById('breakdance').addEventListener("click",()=> lane.breakdance())
+const eatButton = document.getElementById('eat').addEventListener("click",()=> lane.eatCronut())
